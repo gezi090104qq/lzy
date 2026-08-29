7cc78ccee82808c73a4474fc44a140bf463d9ddf
gezi090104qq <3088457780@qq.com>
2026-08-29 21:23:36 +0800

    Add main script and translation for AOTR

---
+++ aotrwudi.lua
@@ -0,0 +1,4 @@
+	-- ① 汉化（先挂上监听）
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/gezi090104qq/lzy/refs/heads/main/AOTRfanyi.txt"))()
+	-- ② Hub 主脚本
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Lixtron/Hub/refs/heads/main/loader"))()
